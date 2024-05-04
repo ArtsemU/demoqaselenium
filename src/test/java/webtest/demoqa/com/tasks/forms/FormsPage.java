5f373f90dd2a65c5757853c81c96436247a25b6f
Artsiom Filatau <artsiom.filatau@geomatrix.group>
2024-05-04 22:33:04 +0200

    First commit for Forms

--- src/test/java/webtest/demoqa/com/tasks/forms/FormsPage.java
+++ src/test/java/webtest/demoqa/com/tasks/forms/FormsPage.java
@@ -5,8 +5,14 @@ import org.openqa.selenium.WebDriver;
 
 public class FormsPage {
     WebDriver driver;
-    private By title = By.xpath("//h1[@class='text-center']");
-    By titlePage = By.xpath("//input[@id='userName']");
+    private By titlePage = By.xpath("//h1[@class='text-center']");
+    private By fname = By.xpath("//input[@id='firstName']");
+    private By lname = By.xpath("//input[@id='lastName']");
+    private By email = By.xpath("//input[@id='userEmail']");
+    private By rbMale = By.xpath("//input[@value='Male']");
+    private By rbFemale = By.xpath("//input[@value='Female']");
+    private By rbOther = By.xpath("//input[@value='Other']");
+    private By mobNum = By.xpath("//input[@id='userNumber']");
     
     public FormsPage(WebDriver driver){
         this.driver = driver;
